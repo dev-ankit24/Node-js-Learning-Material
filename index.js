916a2b4475dbd799cee2e1b2cbf0bead29ebc6e1
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-28 11:30:13 +0530

    CJs Module

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // using CJS  
 
-const name=require("./userdefineModules/test1")
-console.log(name)
+// const name=require("./userdefineModules/test1")
+// console.log(name)
+
+// import from using Array with destacture
+const [name,arr,emp,fun ,Example]=require("./userdefineModules/test1")
+console.log("Name:",name);
+console.log("Array:",arr);
+console.log("employee:",emp);
+fun()
+var obj=new Example
+obj.show()
--- userdefineModules/test1.js
+++ userdefineModules/test1.js
@@ -1,2 +1,25 @@
-var name= "ankit kumar"
-module.exports=name
+// CJS using
+// var name= "ankit kumar"
+// module.exports=name
+
+// multiple varible
+ var name= "ankit kumar"
+ module.exports=name
+ var arr=[10,20,50,80,7]
+ var emp={
+    id:1001,
+    name:"Nitin Chauhan",
+    dsg:"Trainer",
+    salary:182000
+ }
+function fun(){
+    console.log("IN Function fun() ");
+}
+class Example{
+    show(){
+        console.log("in Show() Example Class");
+    }
+}
+module.exports=[
+    name,arr,emp,fun, Example
+]
