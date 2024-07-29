73e3a3079ef416ce8a07d63edc0c8d901e781057
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-29 17:43:59 +0530

    create thirdparty module

--- index.js
+++ index.js
@@ -37,4 +37,33 @@
 // require("./BuiltinModules/fsModuleSync")
 // require("./BuiltinModules/fsModuleAsync ")
 // require("./BuiltinModules/osModule");
-require("./BuiltinModules/pathModule");
+// require("./BuiltinModules/pathModule");
+
+// ************************************ ThirdParty modules***********************************************
+
+// var passwordValidator = require('password-validator');
+// var schema = new passwordValidator();
+
+// schema
+// .is().min(5)                                    // Minimum length 8
+// .is().max(100)                                  // Maximum length 100
+// .has().uppercase(1)                              // Must have uppercase letters
+// .has().lowercase(1)                              // Must have lowercase letters
+// .has().digits(1)                                // Must have at least 2 digits
+// .has().not().spaces()                           // Should not have spaces
+// .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+
+// console.log(schema.validate("ankit123"));                 // false      
+// console.log(schema.validate("ANKIT123"));                 // false
+// console.log(schema.validate("ankit123"));                 // false
+// console.log(schema.validate("Password123"));              // false
+// console.log(schema.validate("Ankit@123"));                //true
+
+
+// server using http  core module
+const http= require("http")
+
+let server= http.createServer((req, res)=>{
+    res.end("Hello from Server ")
+})
+server.listen(8000,console.log("server is running http://localhost:8000"))
