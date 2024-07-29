d5f30b9b142f707aa15fa32591d0610e0251cbcd
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-29 17:04:24 +0530

    path module

--- BuiltinModules/pathModule.js
+++ BuiltinModules/pathModule.js
@@ -1,1 +1,18 @@
 const path= require("path")
+
+let a= "E:\Node\test"
+// return file with extension
+console.log("file name:", path.basename(a));
+
+//return directry name
+console.log("directry name", path.dirname(a));
+
+// return file extension name
+console.log("File Extension:",path.extname(a));
+
+// resolve path (old path change new Path)
+//  console.log(path.resolve("D:\\Nitin\\sample.txt", "D:\\Nitin\\sample9.txt"));
+
+console.log(path.isAbsolute("D:\\Nitin\\sample.txt"));
+console.log(path.isAbsolute("www.google.com"));
+console.log(path.isAbsolute("index.html"));
