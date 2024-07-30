f1d0eeff7d09c4701c81c15e4a6937ce668a5249
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-30 15:00:22 +0530

    create server and upload json data or file

--- index.js
+++ index.js
@@ -61,9 +61,78 @@
 
 
 // server using http  core module
-const http= require("http")
+// const http= require ("http")
 
-let server= http.createServer((req, res)=>{
-    res.end("Hello from Server ")
+// let server= http.createServer((req, res)=>{
+//     res.write("Hello world 1")
+//     res.write("Hello world 2")
+//     res.write("Hello world 3")
+//     res.write("Hello world 4")
+//     res.end("Hello from Server ")
+// })
+// server.listen(8001,console.log("server is running http://localhost:8001"))
+
+
+// Routing in Node js
+// const http= require("http")
+
+// const server= http.createServer((req,res)=>{
+//     if(req.url=="/")
+//         res.end('<h1>Home Page</h1>')
+//     else if (req.url=="/about")
+//         res.end("<h1>About Page</h1>")
+//     else if(req.url=="/contact")
+//         res.end("<h1>Contact  Page</h1>")
+//     else{
+//         res.writeHead(404)
+//         res.end("<h1>404!  Page Not Found</h1>")
+//     }
+// })
+// server.listen(8000,console.log("Server is Runing http:\\localhost:8000"))
+
+
+// serving JSON data from server
+// const http= require ("http")
+
+// let server=http.createServer((req,res)=>{
+//     if(req.url=="/")
+//         res.end("<h4>Home page</h4>")
+//     else if (req.url=="/data"){
+//         let data=[
+//             {id:102,name:"ankit kumar", mob:"8392823395"},
+//             {id:103,name:"paramnand", mob:"9845852552"},
+//             {id:104,name:"Devesh verma", mob:"9544578147"},
+//             {id:105,name:"alok kumar", mob:"7825105842"},
+//             {id:102,name:"manish", mob:"83978247022"},
+//         ]
+//         res.end(JSON.stringify(data))
+//     }
+//     else{
+//         res.writeHead(404)
+//         res.end("<h4> 404! Page Not Found  </h4>")
+//     }
+// })
+
+// server.listen(8000, console.log("http://localhost:8000"));
+
+
+// serving JSON file data from server
+const http= require ("http")
+const fs= require ("fs")
+
+const server = http.createServer((req,res)=>{
+    if(req.url=="/")
+        res.end("<h4>Home Page</h4>")
+    else if(req.url=="/data"){
+        let data=fs.readFileSync("data.json", "utf-8")
+        res.end(data)
+    }
+    else{
+        res.writeHead(404)
+        res.end("<h4>404 Not Page found</h4>")
+    }
 })
-server.listen(8000,console.log("server is running http://localhost:8000"))
+server.listen(8000, console.log("server is Running http://localhost:8000"))
+
+
+
